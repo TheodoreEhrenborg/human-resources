59433fe3a6255d171bbbc7a1774bb2a4807503fe
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2021-07-09 23:06:32 -0400

    Finished untested version

--- lib.py
+++ lib.py
@@ -21,6 +21,47 @@ class Candidate:
     def generate_id():
         return "C" + str(len(Candidate.candidate_list))
 
+    @staticmethod
+    def login_or_register():
+        response = ""
+        while response not in ("y", "n"):
+            response = input(
+                "Have you already registered? (y/n) "
+            ).lower()
+        if response == "y":
+            id_num = input("Your ID: ")
+            c = Candidate.find_candidate_with_id(id_num)
+            print("Hello, " + c.name)
+            return c
+        else:
+            c = Candidate()
+            c.register()
+            return c
+
+    @staticmethod
+    def find_candidate_with_id(id_num):
+        for c in Candidate.candidate_list:
+            if c.id_num == id_num:
+                return c
+        else:
+            raise Exception("ID not found")
+
+    @staticmethod
+    def print_candidates():
+        any = False
+        for c in Candidate.candidate_list:
+            any = True
+            print("ID: " + c.id_num)
+            print("Name: " + c.name)
+            print("Resume: " + c.resume)
+            if len(c.applications) > 0:
+                print("Applications made: ")
+            for a in c.applications:
+                print(a)
+            print()
+        if not any:
+            raise Exception("No candidates")
+
     def __init__(self):
         self.name = ""
         self.id_num = ""
@@ -31,16 +72,64 @@ class Candidate:
         if self.id_num != "":
             raise Exception("Already registered")
         self.name = input("Name: ")
-        self.get_resume()
+        self.make_resume()
         Candidate.add(self)
         self.id_num = Candidate.generate_id()
+        print("Your ID: " + self.id_num)
 
-    def get_resume(self):
+    def make_resume(self):
         self.resume = input("Your resume: ")
 
     def apply(self):
         a = Application()
-        a.make_application(self)
+        try:
+            a.make_application(self)
+        except Exception as e:
+            print(e)
+
+    def list_my_applications(self):
+        Application.list_applications_of(self)
+
+    def list_available_jobs(self):
+        try:
+            Job.print_available_jobs_long()
+        except Exception as e:
+            print(e)
+
+    def interact(self):
+        options = [
+            "exit",
+            "apply",
+            "list",
+            "check",
+            "update",
+            "see",
+        ]
+        while True:
+            print(
+                """Your options are:
+    list --- see a list of available jobs
+    see --- see your resume
+    update --- update your resume
+    check --- check the status of your job applications
+    apply --- apply for a job
+    exit"""
+            )
+            choice = ""
+            while choice not in options:
+                choice = input("Your choice: ").lower()
+            if choice == "list":
+                self.list_available_jobs()
+            elif choice == "see":
+                print(self.resume)
+            elif choice == "update":
+                self.make_resume()
+            elif choice == "check":
+                self.list_my_applications()
+            elif choice == "apply":
+                self.apply()
+            else:
+                break
 
 
 class Job:
@@ -63,6 +152,31 @@ class Job:
                 print("ID: " + job.id_num)
                 print("Title: " + job.title)
                 print()
+        if not any:
+            raise Exception("No jobs available")
+
+    @staticmethod
+    def print_available_jobs_long():
+        any = False
+        for job in Job.job_list:
+            if job.available:
+                any = True
+                print("ID: " + job.id_num)
+                print("Title: " + job.title)
+                print("Description: " + job.description)
+                print()
+        if not any:
+            raise Exception("No jobs available")
+
+    @staticmethod
+    def print_jobs_long():
+        any = False
+        for job in Job.job_list:
+            any = True
+            print("ID: " + job.id_num)
+            print("Title: " + job.title)
+            print("Description: " + job.description)
+            print()
         if not any:
             raise Exception("No jobs available")
 
@@ -109,12 +223,69 @@ class Application:
     def generate_id():
         return "A" + str(len(Application.application_list))
 
+    @staticmethod
+    def list_applications_of(candidate):
+        for a in Application.application_list:
+            print(a)
+            print()
+
+    @staticmethod
+    def print_open_applications():
+        any = False
+        for a in Application.application_list:
+            if a.status not in (
+                Status.REJECTED,
+                Status.ACCEPTED,
+            ):
+                any = True
+                print(a)
+                print()
+        if not any:
+            raise Exception("No open applications")
+
+    @staticmethod
+    def print_applications():
+        any = False
+        for a in Application.application_list:
+            any = True
+            print(a)
+            print()
+        if not any:
+            raise Exception("No open applications")
+
+    @staticmethod
+    def get_application_with_id(id_num):
+        for a in Application.application_list:
+            if a.id_num == id_num:
+                return a
+        raise Exception("No such application found")
+
     def __init__(self):
         self.id_num = ""
         self.candidate = None
         self.job = None
         self.status = Status.APPLIED
         self.interview_transcript = ""
+
+    def __str__(self):
+        return (
+            "ID: "
+            + self.id_num
+            + "\nJob: "
+            + self.job.title
+            + "\nStatus: "
+            + self.status.name.title()
+        )
+
+    def get_details(self):
+        output = str(self)
+        output += "\nCandidate: " + a.candidate.id_num
+        if self.status != Status.APPLIED:
+            output += (
+                "\nInterview transcript: "
+                + a.interview_transcript
+            )
+        return output
 
     def make_application(self, candidate):
         if self.id_num != "":
@@ -178,3 +349,109 @@ class Application:
             )
         self.status = Status.ACCEPTED
         self.job.close_job()
+
+
+def hr_interact():
+    options = [
+        "exit",
+        "make",
+        "close",
+        "list cands",
+        "list apps",
+        "list open apps",
+        "list jobs",
+        "list open jobs",
+        "details",
+        "interview",
+        "accept",
+        "reject",
+    ]
+    while True:
+        print(
+            """Your options are:
+        make --- make a job posting
+        close --- manually close a job posting
+        list cands --- list all candidates
+        list apps --- list all job applications
+        list open apps --- like previous, excluding accepted and rejected ones
+        list jobs --- list all job postings
+        list open jobs --- list all open job postings
+        details --- print details of an application
+        interview --- conduct an interview for an application
+        accept --- accept an application
+        reject --- reject an application
+        exit"""
+        )
+        choice = ""
+        while choice not in options:
+            choice = input("Your choice: ").lower()
+        if choice == "make":
+            j = Job()
+            j.describe()
+        elif choice == "close":
+            try:
+                j = Job.get_job_with_id(input("Job ID: "))
+                j.available = False
+            except Exception as e:
+                print(e)
+        elif choice == "list cands":
+            try:
+                Candidate.print_candidates()
+            except Exception as e:
+                print(e)
+        elif choice == "list apps":
+            try:
+                Application.print_applications()
+            except Exception as e:
+                print(e)
+        elif choice == "list open apps":
+            try:
+                Application.print_open_applications()
+            except Exception as e:
+                print(e)
+        elif choice == "list jobs":
+            try:
+                Job.print_jobs_long()
+            except Exception as e:
+                print(e)
+        elif choice == "list open jobs":
+            try:
+                Job.print_available_jobs_long()
+            except Exception as e:
+                print(e)
+        elif choice == "details":
+            if True:
+                # try:
+                # I got an error when I selected details
+                a = Application.get_application_with_id(
+                    input("Application ID: ")
+                )
+                print(a.get_details())
+        #    except Exception as e:
+        #        print(e)
+        elif choice == "interview":
+            try:
+                a = Application.get_application_with_id(
+                    input("Application ID: ")
+                )
+                a.do_interview()
+            except Exception as e:
+                print(e)
+        elif choice == "accept":
+            try:
+                a = Application.get_application_with_id(
+                    input("Application ID: ")
+                )
+                a.accept()
+            except Exception as e:
+                print(e)
+        elif choice == "reject":
+            try:
+                a = Application.get_application_with_id(
+                    input("Application ID: ")
+                )
+                a.reject()
+            except Exception as e:
+                print(e)
+        else:
+            break
--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
 
-import lib
+from lib import Application, Candidate, Job, hr_interact
+import pickle
+
+try:
+    with open("data.pickle", "rb") as f:
+        (a_list, c_list, j_list) = pickle.load(f)
+except FileNotFoundError:
+    pass
+else:
+    Application.application_list = a_list
+    Candidate.candidate_list = c_list
+    Job.job_list = j_list
+
+print("Make sure only one can run at a time")
+
+answer = 0
+while answer not in (1, 2):
+    answer = int(
+        input(
+            "Type 1 if you're in HR, 2 if you want to apply for a job: "
+        )
+    )
+if answer == 1:
+    hr_interact()
+else:
+    c = Candidate.login_or_register()
+    c.interact()
 
 
-j = lib.Job()
-
-
-j.describe()
-
-
-c = lib.Candidate()
-
-
-c.register()
-
-
-c.apply()
-
-
-a = lib.Application.application_list[0]
+with open("data.pickle", "wb") as f:
+    pickle.dump(
+        (
+            Application.application_list,
+            Candidate.candidate_list,
+            Job.job_list,
+        ),
+        f,
+    )
